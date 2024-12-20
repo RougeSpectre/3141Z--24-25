1afa0e62dae3686ac2220378b589b2cc28b23d56
RougeSpectre <88217282+RougeSpectre@users.noreply.github.com>
2024-12-20 09:42:02 -0600

    12/20/2024
    Created Everything

---
+++ 3141Z--24-25/include/driver.h
@@ -0,0 +1,7 @@
+#ifndef __Driver__
+#define __Driver__
+
+extern void Driver();
+extern void UpdateCScreen();
+
+#endif __Driver__
---
+++ 3141Z--24-25/include/preAuto.h
@@ -0,0 +1,12 @@
+#ifndef __PreAuto__
+#define __PreAuto__
+
+extern void PreAutonomous();
+
+extern void SetupSystems();
+extern int SelectedAuto;
+extern int ControlMode;
+extern int AutoType;
+extern bool Calibrating;
+
+#endif __PreAuto__
